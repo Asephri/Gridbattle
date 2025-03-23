5ff490dd310c8aab451370ea50ba55b3933de134
Asephri <56927504+Asephri@users.noreply.github.com>
2025-03-23 16:09:28 +0000

    Update player.c

--- src/player.c
+++ src/player.c
@@ -2,39 +2,36 @@
 Copyright (C) 2023-2024 Asephri. All rights reserved.
 */
 
-/* Headers. */
 #include "common.h"
 #include "draw.h"
 #include "player.h"
 #include "input.h"
 #include "util.h"
 
-/* Externs. */
 extern App app;
 extern Entity *player;
 extern Entity *playertank;
 extern Stage stage;
 
-/* Initialisng the player. */
 void initPlayer(void)
 {
-    // Console message.
+
     printf("Intialising the player.\n");
-    // Assiging memory blocks.
+
     playertank = malloc(sizeof(Entity));
     memset(playertank, 0, sizeof(Entity));
-    // Add playertank to linked list in stage.
+
     stage.entityTail->next = playertank;
     stage.entityTail = playertank;
 
-    // Assiging memory blocks.
+
     player = malloc(sizeof(Entity));
     memset(player, 0, sizeof(Entity));
-    // Add player to linked list in stage.
+
     stage.entityTail->next = player;
     stage.entityTail = player;
     
-    // Player variables.
+
     playertank->texture = loadTexture("gfx/tank.png");
     player->texture = loadTexture("gfx/playerturret.png");
     player->health = 5;
@@ -44,21 +41,18 @@ void initPlayer(void)
     player->x = playertank->x;
     player->y = playertank->y;
 
-    // Query player texture.
+
     SDL_QueryTexture(player->texture, NULL, NULL, &player->w, &player->h);
     SDL_QueryTexture(playertank->texture, NULL, NULL, &playertank->w, &playertank->h);
-    // Console message.
+
     printf("Successful.\n");
 }
 
-/* Player behaviour. */
 void doPlayer(void)
 {
-    // Decelerating the player by multiplying = smoother movement.
     playertank->dx *= 0.86;
     playertank->dy *= 0.86;
 
-    // Forward
     if (app.keyboard[SDL_SCANCODE_W])
     {   
         double angle_rad = playertank->angle * PI / 180;
@@ -66,7 +60,6 @@ void doPlayer(void)
         playertank->dy = PLAYER_SPEED * sin(angle_rad);
     }
 
-    // Reverse (if needed)
     if (app.keyboard[SDL_SCANCODE_S])
     {
         double angle_rad = playertank->angle * PI / 180;
@@ -74,10 +67,8 @@ void doPlayer(void)
         playertank->dy = -PLAYER_SPEED * sin(angle_rad) / 4;
     }
 
-    // Rotation using A and D keys.
     if (app.keyboard[SDL_SCANCODE_A])
     {   
-        // Ensure the angle stays within the range of 0 to 359 degrees.
         playertank->angle = playertank->angle - ROTATION_ANGLE_SPEED;
     }
     else if (app.keyboard[SDL_SCANCODE_D])
@@ -85,11 +76,9 @@ void doPlayer(void)
         playertank->angle = playertank->angle + ROTATION_ANGLE_SPEED;
     }
 
-    // Update the tank's position
     playertank->x += playertank->dx;
     playertank->y += playertank->dy;
 
-    // Update the tank's turret position.
     player->x = playertank->x + (playertank->w - player->w) / 2.0;
     player->y = playertank->y + (playertank->h - player->h) / 2.0;
 
